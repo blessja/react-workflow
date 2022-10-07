106e6fadb406727e4034722721aa00285e6c93ce
blessja <jaydenj772@gmail.com>
2022-10-07 02:42:41 +0200

    add background, add controls and minimap

---
+++ src/App/index.tsx
@@ -0,0 +1,15 @@
+import Flow from '../Flow';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" style={{color: 'white'}}>React workflow Assistant</header>
+      <Flow />
+      
+    </div>
+  );
+}
+
+export default App;
